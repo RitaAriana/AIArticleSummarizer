b7f87a1ee8048a733ba4bfe0b865f4ae987980a1
Rita Ariana <1201386@isep.ipp.pt>
2023-10-24 18:05:18 +0100

    Hero Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,26 @@ import logo from '../assets/logo.svg'
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-        <nav className='flex justify-between items-center flex-col'>
+        <nav className='flex justify-between items-center w-full mb-10 pt-3'>
             <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+            <button 
+                type='button'
+                onClick={() => window.open("https://github.com/RitaAriana")}
+                className='black_btn'>
+                    GitHub
+            </button>
         </nav>
+
+        <h1 className='head_text'>
+            Summarize Articles with <br className='max-md:hidden' />
+            <span className='orange_gradient'>Open GPT-4</span>
+        </h1>
+
+        <h2 className='desc'>
+            Simplify your reading with our AI-powered summarizer, 
+            an open-source article summarizer that transforms long articles into concise summaries. 
+        </h2>
+
     </header>
   )
 }
